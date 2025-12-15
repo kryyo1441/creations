fc19c2eeaae56c8a52a49cbfb70251e309184603
Apekshat69 <tandelapeksha97@gmail.com>
2025-12-16 00:22:17 +0530

    Hero section hahah

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
-import Image from "next/image";
-
+import HeroSection from '@/components/landing-page/hero-page';
 export default function Home() {
   return (
-    <div>
-      <Badge>Join the community to learn from creations!</Badge>
-      
-      <h1>You're creations needs the perfect audience, Discover other creations</h1>
-      <p>
-        Showcase your programming creations, discover others’ work, and let the community decide with likes and dislikes.
-      </p>
-      <Button>Share your projects</Button>
-      <Button>Explore other projects</Button>
-
-    </div>
-
+    <HeroSection/>
 
   );
 }
---
+++ components/landing-page/hero-page.tsx
@@ -0,0 +1,19 @@
+import { Badge } from "../ui/badge";
+import { Button } from "../ui/button";
+
+export default function Hero() {
+  return (
+    <div>
+      <Badge>Join the community to learn from creations!</Badge>
+      
+      <h1>You're creations needs the perfect audience, Discover other creations</h1>
+      <p>
+        Showcase your programming creations, discover others’ work, and let the community decide with likes and dislikes.
+      </p>
+      <Button>Share your projects</Button>
+      <Button>Explore other projects</Button>
+
+    </div>
+
+  );
+}
