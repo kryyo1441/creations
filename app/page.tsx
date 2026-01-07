cd2646d0b905003ca4bf355693f4350a3a6d61c6
kryyo1441 <aayushnair321@gmail.com>
2026-01-07 23:37:00 +0530

    Added shadcn cards for trending products page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import HeroSection from '@/components/landing-page/hero-page';
+import TrendingProducts from '@/components/landing-page/trending-products';
 export default function Home() {
   return (
-    <HeroSection/>
-
+    <>
+      <HeroSection/>
+      <TrendingProducts />
+    </>
   );
 }
---
+++ components/landing-page/trending-products.tsx
@@ -0,0 +1,7 @@
+export default function TrendingProducts() {
+  return (
+    <>
+      <h1>hi</h1>
+    </>
+  );
+}
