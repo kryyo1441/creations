78cbbd6baa898f06a5ef6e7e8ae2685d7beeadba
kryyo1441 <aayushnair321@gmail.com>
2026-01-08 22:21:36 +0530

    trending products page

--- components/landing-page/trending-products.tsx
+++ components/landing-page/trending-products.tsx
@@ -1,7 +1,13 @@
 export default function TrendingProducts() {
   return (
     <>
-      <h1>hi</h1>
+    <section className="py-20 bg-muted/30">
+      <div className="wrapper">
+        <div className="flex items-center justify-between mb-8">
+          <h1>Trending products</h1>
+        </div>
+      </div>
+    </section>
     </>
   );
 }
