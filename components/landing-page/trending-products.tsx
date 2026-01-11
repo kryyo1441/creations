fb033a59d77e990842e59cd7c904b28795f573dc
kryyo1441 <aayushnair321@gmail.com>
2026-01-11 21:22:00 +0530

    VIew All button

--- components/landing-page/trending-products.tsx
+++ components/landing-page/trending-products.tsx
@@ -1,5 +1,7 @@
-import { StarIcon } from "lucide-react";
+import { ArrowUpRightIcon, StarIcon } from "lucide-react";
+import Link from "next/link";
 import SectionHeader from "../common/section-header";
+import { Button } from "../ui/button";
 
 export default function TrendingProducts() {
   return (
@@ -8,6 +10,14 @@ export default function TrendingProducts() {
       <div className="wrapper">
         <div className="flex items-center justify-between mb-8">
           <SectionHeader title="Trending Products"  icon={StarIcon} description="Top creations from our community"/>
+            <Button 
+            className="hidden sm:flex" 
+            variant="outline" 
+            asChild>
+            <Link href="/explore">
+              View All <ArrowUpRightIcon className="size-4"/>
+            </Link>
+            </Button>
         </div>
       </div>
     </section>
