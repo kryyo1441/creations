67646be5565badd4b9e70720ab7b30431a83ab97
kryyo1441 <aayushnair321@gmail.com>
2026-01-12 21:36:53 +0530

    recently launced section header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from '@/components/landing-page/hero-page';
+import RecentlyLaunchedProducts from '@/components/landing-page/recently-launced';
 import TrendingProducts from '@/components/landing-page/trending-products';
 export default function Home() {
   return (
     <>
       <HeroSection/>
       <TrendingProducts />
+      <RecentlyLaunchedProducts />
     </>
   );
 }
