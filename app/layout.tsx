4bb158fe8b2af908f459516136c2de4aef03314e
kryyo1441 <aayushnair321@gmail.com>
2026-01-13 21:24:49 +0530

    Linked to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/common/header";
+import Footer from "@/components/common/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       >
         <Header></Header>
         {children}
+        <Footer></Footer>
       </body>
     </html>
   );
