b5ae241ec5c8085251f8527ef07d4931b553ca14
kryyo1441 <aayushnair321@gmail.com>
2026-01-20 19:41:14 +0530

    Auth sign in and sign up function added from clerk

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -2,6 +2,7 @@ import { HandMetal, HomeIcon, Rocket, SparklesIcon, UserIcon } from "lucide-reac
 import Link from "next/link"
 import { Button } from "../ui/button"
 import { SignIn, SignUp } from "@clerk/nextjs"
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs"
 
 
 const Logo = () => {
@@ -17,44 +18,49 @@ const Logo = () => {
     )
 }
 
-export default function Header(){
-    const isSignedin = false;
-    return(
+export default function Header() {
+    const isSignedin = true;
+    
+    return (
         <header className="sticky top-0 z-50 border-b border-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
-            <div className="wrapper px-12 ">
+            <div className="wrapper px-12">
                 <div className="flex h-15 items-center justify-between">
-                <Logo></Logo>
-                <nav className="flex items-center gap-1">
-                    <Link href="/" className="flex items-center gap-2 px-3 py-2 text-sm font-medium text-muted-foreground hover:text-foreground transition-colors hover:bg-muted/50">
-                        <HomeIcon className="size-4"></HomeIcon>
-                        <span>Home</span>
-                    </Link>
-                    <Link href="/" className="flex items-center gap-2 px-3 py-2 text-sm font-medium text-muted-foreground hover:text-foreground transition-colors hover:bg-muted/50">
-                        <Rocket className="size-4"></Rocket>
-                        <span>Explore</span>
-                    </Link>
-                    <div className="flex items-center gap-4">
-                        {isSignedin ? (
-                            <>
-                                <Button asChild>
-                                    <Link href="/submit">
-                                        <SparklesIcon className="size-4"/>
-                                        <span>Submit Creation</span>
-                                    </Link>
+                    <Logo />
+                    <nav className="flex items-center gap-1">
+                        <Link href="/" className="flex items-center gap-2 px-3 py-2 text-sm font-medium text-muted-foreground hover:text-foreground transition-colors hover:bg-muted/50">
+                            <HomeIcon className="size-4" />
+                            <span>Home</span>
+                        </Link>
+                        <Link href="/" className="flex items-center gap-2 px-3 py-2 text-sm font-medium text-muted-foreground hover:text-foreground transition-colors hover:bg-muted/50">
+                            <Rocket className="size-4" />
+                            <span>Explore</span>
+                        </Link>
+                        <div className="flex items-center gap-4">
+                            <SignedOut>
+                                <Button>
+                                    <SignInButton />
                                 </Button>
-                                <Button variant="ghost"><UserIcon className="size-4"></UserIcon></Button>
-                            </>
-                        ) : (
-                            <>
-                                <SignIn/>
-                                <SignUp/>
-                                <Button>Sign up</Button>
-                            </>
-                        )}
-                    </div>
-                </nav>
+                                <SignUpButton>
+                                    <Button>Sign Up</Button>
+                                </SignUpButton>
+                            </SignedOut>
+                            <SignedIn>
+                                 <>
+                                    <Button asChild>
+                                        <Link href="/submit">
+                                            <SparklesIcon className="size-4" />
+                                            <span>Submit Creation</span>
+                                        </Link>
+                                    </Button>
+                                    
+                                </>
+                                <UserButton />
+                            </SignedIn>
+                           
+                        </div>
+                    </nav>
                 </div>
             </div>
         </header>
-    )
+    );
 }
