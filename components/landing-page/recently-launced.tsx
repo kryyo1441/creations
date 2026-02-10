b8fbe71cf89575fe6c2915653c3667a2976aa031
kryyo1441 <aayushnair321@gmail.com>
2026-02-10 17:44:36 +0530

    Fetch based on date

--- components/landing-page/recently-launced.tsx
+++ components/landing-page/recently-launced.tsx
@@ -2,9 +2,10 @@ import { CalendarIcon, RocketIcon, Section } from "lucide-react";
 import SectionHeader from "../common/section-header";
 import ProductCard from "../products/product-card";
 import EmptyState from "../common/empty-state";
+import { getRecentlyLaunchedProducts } from "@/lib/products/product-select";
 
-export default function RecentlyLaunchedProducts() {
-    const recentlyLauncedCreations = []
+export default async function RecentlyLaunchedProducts() {
+    const recentlyLauncedCreations = await getRecentlyLaunchedProducts();
     return(
         <>
             <section className="py-20 bg-muted/20">
